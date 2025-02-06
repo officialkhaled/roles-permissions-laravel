4035293f34efd7dcf234254aae389a7766bc0e01
Khaled Hossain <khaledhossain778@gmail.com>
2025-02-06 22:14:45 +0600

    project initialization

---
+++ database/seeders/UserRolePermissionSeeder.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserRolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create Permissions
+        Permission::create(['name' => 'view role']);
+        Permission::create(['name' => 'create role']);
+        Permission::create(['name' => 'update role']);
+        Permission::create(['name' => 'delete role']);
+
+        Permission::create(['name' => 'view permission']);
+        Permission::create(['name' => 'create permission']);
+        Permission::create(['name' => 'update permission']);
+        Permission::create(['name' => 'delete permission']);
+
+        Permission::create(['name' => 'view user']);
+        Permission::create(['name' => 'create user']);
+        Permission::create(['name' => 'update user']);
+        Permission::create(['name' => 'delete user']);
+
+        Permission::create(['name' => 'view product']);
+        Permission::create(['name' => 'create product']);
+        Permission::create(['name' => 'update product']);
+        Permission::create(['name' => 'delete product']);
+
+
+        // Create Roles
+        $superAdminRole = Role::create(['name' => 'super-admin']); //as super-admin
+        $adminRole = Role::create(['name' => 'admin']);
+        $staffRole = Role::create(['name' => 'staff']);
+        $userRole = Role::create(['name' => 'user']);
+
+        $allPermissionNames = Permission::pluck('name')->toArray();
+
+        $superAdminRole->givePermissionTo($allPermissionNames);
+
+        $adminRole->givePermissionTo(['create role', 'view role', 'update role']);
+        $adminRole->givePermissionTo(['create permission', 'view permission']);
+        $adminRole->givePermissionTo(['create user', 'view user', 'update user']);
+        $adminRole->givePermissionTo(['create product', 'view product', 'update product']);
+
+
+        $superAdminUser = User::firstOrCreate([
+            'email' => 'superadmin@gmail.com',
+        ], [
+            'name' => 'Super Admin',
+            'email' => 'superadmin@gmail.com',
+            'password' => Hash::make('123456'),
+        ]);
+
+        $superAdminUser->assignRole($superAdminRole);
+
+
+        $adminUser = User::firstOrCreate([
+            'email' => 'admin@gmail.com'
+        ], [
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('123456'),
+        ]);
+
+        $adminUser->assignRole($adminRole);
+
+
+        $staffUser = User::firstOrCreate([
+            'email' => 'staff@gmail.com',
+        ], [
+            'name' => 'Staff',
+            'email' => 'staff@gmail.com',
+            'password' => Hash::make('123456'),
+        ]);
+
+        $staffUser->assignRole($staffRole);
+    }
+}
